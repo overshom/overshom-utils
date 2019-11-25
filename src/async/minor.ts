a5e6a46ceb3443e9c5b9632fde40beaf207e1b6d
Oleksii Shnyra <shnura94@gmail.com>
2019-11-25 10:19:57 +0200

    refactor(sleep): better semantics

--- src/async/minor.ts
+++ src/async/minor.ts
@@ -1,4 +1,4 @@
 /**
  * Asynchronously await for desired ms time
  */
-export const sleep = (ms: number) => new Promise(r => setTimeout(r, ms))
+export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
